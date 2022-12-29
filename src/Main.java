fdcaa668e01b9fb9ec3d6b5e150873b6c24cfb47
GIDRO32 <pambossable@gmail.com>
2022-12-29 13:55:02 +0200

    Add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args)
+    {
+    NumberSum();
+    }
+    public static void NumberSum()
+    {
+        System.out.println(NumberOne(3) + NumberTwo(5));
+    }
+    public static int NumberOne(int numOne)
+    {
+        return numOne;
+    }
+    public static int NumberTwo(int numTwo)
+    {
+        return numTwo;
     }
 }
